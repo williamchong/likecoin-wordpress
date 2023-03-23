410801e3003c9819f1d64f82a4c5077d9caac040
William Chong <me@williamchong.cloud>
2023-03-24 01:15:28 +0800

    ✨ Use app.like.co as widget

--- likecoin/assets/js/sidebar/index.asset.php
+++ likecoin/assets/js/sidebar/index.asset.php
@@ -1,4 +1,4 @@
 <?php return array(
 	'dependencies' => array( 'react', 'wp-api-fetch', 'wp-data', 'wp-edit-post', 'wp-element', 'wp-i18n', 'wp-plugins', 'wp-wordcount' ),
-	'version'      => 'cdcb2fbacc74ecfac97e2a83ca81cecd',
+	'version'      => 'f6da4f646c2a4773be15fb5f94a5e4d3',
 );
